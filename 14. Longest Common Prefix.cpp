d76f761bfd9b216b3182ca15811d2de8ec32a0c0
Fahmid-Bin-Mosaraf <fahmidfamel396@gmail.com>
2025-03-19 23:54:31 +0600

    new commit

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+        
+        string s = strs[0];
+        for(int i=1; i<strs.size(); i++){
+            while(strs[i].find(s) != 0) {
+                s = s.substr(0, s.length() - 1);
+                if(s.empty()){
+                    return "";
+                }
+            }
+        }
+        return s;
+    }
+};
+
+
+// Time Complexity: O(n*m)
+// Space Complexity: O(1)
