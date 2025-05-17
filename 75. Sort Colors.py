79751c6062c1c6a09db2e509c67fe649a81cb9ff
Fahmid-Bin-Mosaraf <fahmidfamel396@gmail.com>
2025-05-17 23:14:55 +0600

    new commit

---
+++ 75. Sort Colors.py
@@ -0,0 +1,12 @@
+# Date: 17/05/2025
+# 75. Sort Colors
+# Python Solution
+# Sorting
+
+class Solution(object):
+    def sortColors(self, nums):
+        nums.sort()
+
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
+            
